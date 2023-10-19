c2d0f8877fc47aaa82a9a04499ee180716a785c6
djc2250 <djc2250@g.rit.edu>
2023-10-19 10:29:40 -0400

    Created PostgresSSH connection and gitignore for logincredentials

---
+++ PostgresSSH.py
@@ -0,0 +1,35 @@
+import psycopg2
+from sshtunnel import SSHTunnelForwarder
+from dotenv import dotenv_values
+
+config = dotenv_values("LoginCredentials.env")
+
+username = config["USERNAME"]
+password = config["PASSWORD"]
+dbName = "p320_09"
+
+try:
+    with SSHTunnelForwarder(('starbug.cs.rit.edu', 22),
+                            ssh_username=username,
+                            ssh_password=password,
+                            remote_bind_address=('127.0.0.1', 5432)) as server:
+        server.start()
+        print("SSH tunnel established")
+        params = {
+            'database': dbName,
+            'user': username,
+            'password': password,
+            'host': 'localhost',
+            'port': server.local_bind_port
+        }
+
+
+        conn = psycopg2.connect(**params)
+        curs = conn.cursor()
+        print("Database connection established")
+
+        #DB work here....
+
+        conn.close()
+except:
+    print("Connection failed")
