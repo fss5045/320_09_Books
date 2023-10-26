134404065407d9f61202f182642fdad36319a53c
Finn <fss5045@rit.edu>
2023-10-26 10:00:43 -0400

    added stubs for apllication functions

--- PostgresFunctions.py
+++ PostgresFunctions.py
@@ -1,3 +1,42 @@
 def showBook(curs):
     curs.execute("SELECT * FROM book")
     return curs.fetchall()
+
+def createNewUser(username, password):
+    return
+
+def followUser(username1, username2):
+    return
+
+def unfollowUser(username1, username2):
+    return
+
+def createCollection(books, name):
+    return
+
+def showCollections(username):
+    return
+
+def modifyCollectionName(collectionId, username):
+    return
+
+def deleteCollection(collectionId, username):
+    return
+
+def searchBooks(name, releaseDate, authors, publishers, genre):
+    return
+
+def filterBooksByName(books):
+    return
+
+def filterBooksByReleaseDate(books, ascending: bool):
+    return
+
+def filterBooksByGenre(books, genre):
+    return
+
+def addBookToCollection(collectionId, book):
+    return
+
+def deleteBookFromCollection(collectionId, book):
+    return
