fa380dc76b05482284621a4825cd45715f718014
Finn <fss5045@rit.edu>
2023-10-30 15:23:23 -0400

    fixed merges

--- PostgresFunctions.py
+++ PostgresFunctions.py
@@ -24,17 +24,39 @@ def unfollowUser(curs, username1, username2):
     curs.execute("DELETE FROM userfollow WHERE usernamefollowed = %s AND usernamefollower = %s", (username1, username2))
     return
 
-def createCollection(curs, books, name):
+def alreadyExistingUser(curs, username):
+    print(curs.mogrify("SELECT * FROM users WHERE EXISTS (SELECT 1 FROM users WHERE users.username = %s)", (username,)))
+    curs.execute("SELECT * FROM users WHERE EXISTS (SELECT 1 FROM users WHERE users.username = %s)", (username,))
+    if(curs.fetchone() == None):
+        return False
+    return True
+
+def userMatchPassword(curs, username, password):
+    print(curs.mogrify("SELECT * FROM users WHERE EXISTS (SELECT 1 FROM users WHERE users.username = %s AND users.password = %s)", (username,password)))
+    curs.execute("SELECT * FROM users WHERE EXISTS (SELECT 1 FROM users WHERE users.username = %s AND users.password = %s)", (username,password))
+    if (curs.fetchone() == None):
+        return False
+    return True
+
+def createCollection(curs, books, name, username):
+    curs.execute("INSERT INTO collection (name, username) VALUES (%s, %s)", (name, username))
+    curs.execute("SELECT FROM collection WHERE name = %s AND username = %s", (name, username))
+    collectionId = curs.fetchone()[0]
+    for book in books:
+        curs.execute("INSERT INTO belongsto (collectionid, book) values (%s, %s)", (collectionId, book))
     return
 
 def showCollections(curs, username):
+    curs.execute("SELECT FROM collection WHERE username = %s" , (username))
     return
 
-def modifyCollectionName(curs, collectionId, username):
+def modifyCollectionName(curs, collectionId, username, newCollectionName):
+    curs.execute("UPDATE collection SET name = %s WHERE collectionId = %s AND username = %s", (newCollectionName, collectionId, username))
     return
 
 def deleteCollection(curs, collectionId, username):
-    return
+    curs.execute("DELETE FROM collection WHERE collectionId = %s AND username = %s" , (collectionId, username))
+    return 
 
 def searchBooks(curs, searchDict):
     query = "SELECT B.bookid, B.title, B.authors, B.publishers, B.length, B.audience FROM book B UNION SELECT R.bookid, R.rating FROM rates R WHERE B.bookid = R.bookid"
@@ -58,7 +80,9 @@ def sortBooksByReleaseYear(curs, quey, ascending: bool):
     return
 
 def addBookToCollection(curs, collectionId, book):
+    curs.execute("INSERT INTO belongsto (collectionid, book) values (%s, %s)", (collectionId, book))
     return
 
 def deleteBookFromCollection(curs, collectionId, book):
+    curs.execute("DELETE FROM belongsto WHERE collectionid=%s AND book=%s", (collectionId, book))
     return
